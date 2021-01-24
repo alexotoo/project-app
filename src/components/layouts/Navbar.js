ce79d47bc6f2f4c4eb449443cc01d531d1bca05b
Alex Otoo <alexotoous@outlook.com>
2021-01-24 01:24:36 +0000

    nav lint

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 import SignedIn from "./SignedIn";
 import SignedOut from "./SignedOut";
 
